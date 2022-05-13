fb58f753302b4950e888343733481677114879a8
text-code <dssn2102@gmail.com>
2022-05-13 17:49:15 +0900

    problem with sequence

--- src/main/kotlin/ChatService.kt
+++ src/main/kotlin/ChatService.kt
@@ -12,9 +12,8 @@ object ChatService {
         chats.forEach { chat -> if (chat.key == key) chats.remove(key) }
     }
 
-    fun getChatList(user: User<Int, String>): Map<Pair<Int, Int>, MutableList<Message<Int, String>>> {
-        return chats.filterKeys { key -> key.first == user.id || key.second == user.id }
-    }
+    fun getChatList(user: User<Int, String>) =
+        chats.filterKeys { it.first == user.id || it.second == user.id }
 
     fun addMessage(writeId: Int, readId: Int, text: String) {
         val key = if (writeId < readId) Pair(writeId, readId) else Pair(readId, writeId)
@@ -28,13 +27,15 @@ object ChatService {
 
     fun removeMessage(firstId: Int, secondId: Int, messageId: Int) {
         val key = if (firstId < secondId) Pair(firstId, secondId) else Pair(firstId, secondId)
-        chats.onEach { (k, v) ->
-            if (k == key && v.size > 1 && v.size > messageId)
-                v.removeAt(messageId)
-            else if (v.size == 1)
-                chats.remove(key)
-            else throw NotFoundException("Message id not found")
-        }
+        chats
+            .filterKeys { it == key }
+            .onEach {
+                if (it.value.size > 1 && it.value.size > messageId)
+                    it.value.removeAt(messageId)
+                else if (it.value.size == 1)
+                    chats.remove(key)
+                else throw NotFoundException("Message id not found")
+            }
 
 //        when {
 //            (chats[key]!!.size > 1 && chats[key]!!.size > messageId) ->
--- src/main/kotlin/UserService.kt
+++ src/main/kotlin/UserService.kt
@@ -4,16 +4,18 @@ import ChatService.addMessage
 import ChatService.removeMessage
 import ChatService.addChat
 import ChatService.removeChat
+import UserService.addMessage
+import UserService.getChats
+import UserService.getMessages
 
 object UserService {
-    fun User<Int, String>.getChats(): String {
-        return getChatList(this)
+    fun User<Int, String>.getChats() =
+        getChatList(this)
             .filterValues { (message) -> !message.readability }
             .toList()
             .joinToString("\n") { (key, value) ->
                 "Чат: $key непрочитанных сообщений: ${value.size}"
             }
-    }
 
     fun User<Int, String>.getMessages(
         secondId: Int,
@@ -45,18 +47,17 @@ object UserService {
 }
 
 
-//fun main() {
-//    val user = User(1, "user1")
-//    val userSecond = User(2, "user2")
-//    val userThread = User(3, "user3")
-//    user.addMessage(userSecond.id, "(1 2) 1")
-//    userSecond.addMessage(user.id, "(2 1) 1")
-//    user.addMessage(userSecond.id, "(1 2) 2")
-//    userSecond.addMessage(user.id, "(2 1) 2")
-//    userThread.addMessage(user.id, "(3 1) 1")
-//
+fun main() {
+    val user = User(1, "user1")
+    val userSecond = User(2, "user2")
+    val userThread = User(3, "user3")
+    user.addMessage(userSecond.id, "(1 2) 1")
+    userSecond.addMessage(user.id, "(2 1) 1")
+    user.addMessage(userSecond.id, "(1 2) 2")
+    userSecond.addMessage(user.id, "(2 1) 2")
+    userThread.addMessage(user.id, "(3 1) 1")
+
 //    println(user.getMessages(userSecond.id, 0, 2))
-//
-//    val result = user.getChats()
-//    println(result)
-//}
+
+    println(user.getChats())
+}
